8c67e205ce0a397773c1047abcb5fde7a8646021
Ka Chan <luandeka@gmail.com>
2018-12-31 15:00:37 -0500

    convenience changing
    DID IT OWKR

--- src/main/java/MainRunner.java
+++ src/main/java/MainRunner.java
@@ -16,8 +16,7 @@ public class MainRunner {
 
     public static void main(String[] args) {
 		Scanner diddleboard = new Scanner(System.in);
-		//String token  = System.getenv("bot_token");
-		String token = args[0];
+		String token  = System.getenv("bot_token");
 		try {
 			IDiscordClient cli = BotUtils.getBuiltDiscordClient(token);
 			// Register a listener via the EventSubscriber annotation which allows for organisation and delegation of events
